2be6d984f08e91d9d285116f8f2097c093213d9c
unknown <masamitsu.mori@gmail.com>
2023-12-12 03:30:39 +0900

    move forest.py

---
+++ src/nu_reactions/event_generator.py
@@ -0,0 +1,32 @@
+from abc import ABCMeta, abstractmethod, ABC
+
+class event_generator(metaclass=ABCMeta):
+
+    @abstractmethod
+    def get_eventrate(self, react_name:str):
+        """
+            returns eventrate of the reaction
+                retrun array of eventrate
+        """
+        pass
+
+    @abstractmethod
+    def get_tot_events(self, react_name:str, flavor:int, t_end:float):
+        """
+            returns total of the reaction
+                retrun array of total events
+        """
+        pass
+
+    @abstractmethod
+    def get_events(self, t_start:float, t_end:float, react_names=[]):
+        """
+            returns arrays of individual events
+                retrun array of events
+        """
+
+    @abstractmethod
+    def get_react_names(self):
+        """
+                retrun array of reaction names
+        """
---
+++ src/nu_reactions/nu_reaction.py
@@ -0,0 +1,58 @@
+from abc import ABCMeta, abstractmethod, ABC
+import numpy as np
+import typing
+
+class nu_reaction(metaclass=ABCMeta):
+    """Abstract class for calculating neutrino reactions"""
+
+    @abstractmethod
+    def get_reaction_name(self) -> str:
+        pass
+
+    @abstractmethod
+    def dcs_nue(self, Enu, E_target):
+        pass
+
+    @abstractmethod
+    def dcs_cos_nue(self, Enu, cos):
+        pass
+
+    @abstractmethod
+    def cs_nue(self, Enu):
+        pass
+
+    @abstractmethod
+    def dcs_anue(self, Enu, E_target):
+        pass
+
+    @abstractmethod
+    def dcs_cos_anue(self, Enu, cos):
+        pass
+
+    @abstractmethod
+    def cs_anue(self, Enu):
+        pass
+
+    @abstractmethod
+    def dcs_nux(self, Enu, E_target):
+        pass
+
+    @abstractmethod
+    def dcs_cos_nux(self, Enu, cos):
+        pass
+
+    @abstractmethod
+    def cs_nux(self, Enu):
+        pass
+    
+    @abstractmethod
+    def cs_table_anue(self, n_cos:int=200, n_e:int=300, E_min:float=0.0, E_max:float=300) -> typing.List[np.array]:
+        pass
+
+    @abstractmethod
+    def get_PID1(self) ->int:
+        pass
+
+    @abstractmethod
+    def get_outgoing_particle_energy(self, cos:float, Enu:float) -> float:
+        pass
